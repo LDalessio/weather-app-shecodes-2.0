6b8608cbd87c4083ee61e1723b5b0958bafba2e9
LDalessio <dalessiomlauren@gmail.com>
2023-08-23 20:18:39 -0700

    Removed metric conversion

--- src/app.js
+++ src/app.js
@@ -154,43 +154,17 @@ function getCurrentLocation(event) {
 let currentButton = document.querySelector("#current-button");
 currentButton.addEventListener("click", getCurrentLocation);
 
-// Temperature Unit Conversion
-//----------------------------------------------
 function handleFormSubmit(event) {
   event.preventDefault();
   let city = document.querySelector("#city-input").value;
   getWeatherByCity(city);
 }
 
-function handleFahrenheitLink(event) {
-  event.preventDefault();
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-  let temperatureElement = document.querySelector("#temperature-value");
-  temperatureElement.innerHTML = Math.round(fahrenheitLinkTemperature);
-}
-
-function handleCelsiusLinkClick(event) {
-  event.preventDefault();
-  celsiusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
-  let temperatureElement = document.querySelector("#temperature-value");
-  let celsiusTemperature = ((fahrenheitLinkTemperature - 32) * 5) / 9;
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-}
-
 // Event Listeners
 //----------------------------------------------
-let fahrenheitLinkTemperature = null;
 
 let searchForm = document.querySelector("#enter-location");
 searchForm.addEventListener("submit", handleFormSubmit);
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", handleFahrenheitLink);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", handleCelsiusLinkClick);
 
 // Initialize with Default City
 //----------------------------------------------
